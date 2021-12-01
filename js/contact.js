356548e3d71eb75ad277542bd28d2e562fd939b6
barbaramootian <barbaramootian@student.moringaschool.com>
2021-12-01 23:53:12 +0300

    add js functionality

--- js/contact.js
+++ js/contact.js
@@ -1,8 +1,7 @@
 function validateform() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var message = document.getElementById("comment").value;
+    var message = document.getElementById("message").value;
     if (email !== "" && name !== "" && message !== "") {
         alert(
             "Hello" + "" + ""  + name +  " Thanks for reaching out to Endless Events. We have received your Message!."
