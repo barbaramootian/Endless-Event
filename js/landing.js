d33c740abe92ade1c180324a1badda6e7c6d2ec8
updhull <abdullahi.ismail@student.moringaschool.com>
2021-12-02 13:53:13 +0300

    Second commit

---
+++ js/landing.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+    $("#image").mouseover(function(){
+        $(".image").show();
+    });
+    $("#image").mouseout(function(){
+        $(".image").hide();
+    });
+    $("#image1").mouseover(function(){
+        $(".image-1").show();
+    });
+    $("#image1").mouseout(function(){
+        $(".image-1").hide();
+    });
+    $("#image2").mouseover(function(){
+        $(".image-2").show();
+    });
+    $("#image2").mouseout(function(){
+        $(".image-2").hide();
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*$(document).ready(function(){
+    $(".small").mouseover(function(){
+        $("#small").show();
+    });
+    $(".small").mouseout(function(){
+        $("#small").hide();
+    });
+
+    $(".medium").mouseover(function(){
+        $("#medium").show();
+    });
+    $(".medium").mouseout(function(){
+        $("#medium").hide();
+    });
+    
+    $(".large").mouseover(function(){
+        $("#large").show();
+    });
+    $(".large").mouseout(function(){
+        $("#large").hide();
+    });
+    
+}); */
