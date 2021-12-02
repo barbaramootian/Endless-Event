6e453b8b463602899a97f7440532cc3d6ebaee6e
amon-wanyonyi <amon.wanyonyi@moringaschool.com>
2021-12-02 08:26:33 +0300

    Rename files

---
+++ js/event.js
@@ -0,0 +1,10 @@
+$("#submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var event = $("#event").val();
+    var event_date = $("#event_date").val();
+    // var str = "You Have Entered " +
+    //     "Name: " + name +
+    //     " and Marks: " + marks;
+    // $("#modal_body").html(str);
+});
